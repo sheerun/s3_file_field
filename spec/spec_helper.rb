0991ef3a146e6f01cf554f0ee8a48f35f091a2a4
Adam Stankiewicz <sheerun@sher.pl>
2013-07-12 05:41:41 +0200

    Remove specs as they are shit

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,10 +1,6 @@
 require 'rubygems'
 require 'bundler/setup'
 
-
-require 'active_support/all'
-require 's3_direct_upload'
-
 RSpec.configure do |config|
   config.treat_symbols_as_metadata_keys_with_true_values = true
   config.run_all_when_everything_filtered = true
